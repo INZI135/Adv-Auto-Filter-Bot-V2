70eeb966023c8b82f218e33fea28ec905ea58ffa
INZI135 <87939669+INZI135@users.noreply.github.com>
2021-07-25 07:55:44 -0700

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,13 +5,9 @@
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
-
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
-
-For More Details Click Help Button Below..
-@CrazyBotsz
-</i>"""    
+I'm Group Managing Bot Exclusively Made For @CinemaCompanyOFFICIAL
+ 
+Join Our Movies Request Group 👉👉 @cinemacompany_officials 👈👈 For Unlimited Movies Collections"""    
     
     HELP_TEXT = """
 <b><i><u>How To Use Me!?</u></i></b>
@@ -43,16 +39,8 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@M_STER_TECH
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
-    
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
-
-<b>➥ Language</b> : <code>Python3</code>
-
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
-
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+    ABOUT_TEXT = """made by @M_STER_TECH
 """
